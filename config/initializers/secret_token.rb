679e156c1b1ea22ac00a01cbd812cc8277c7eda2
Matt Cano <cano.matt@gmail.com>
2013-05-16 15:30:25 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nike::Application.config.secret_token = '0977689c5685c0c2e8ab5c3e369b24d0cce9984c27e4510de79ff2841fb548f08f450a72ae7738e3d2058033a5530a737e5dfa80a531f1d370b2b31591857753'
